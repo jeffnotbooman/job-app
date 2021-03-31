e4ba3d2b91173a86842fdefbd8e544d1012df047
haryor745 <76107362+haryor745@users.noreply.github.com>
2021-03-31 22:32:17 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+    </>
+  );
+}
+export default App;
---
+++ src/components/Body.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import close from "../images/icon-remove.svg";
+import array from "../data";
+export default function Lists() {
+  const [data, setData] = useState(array);
+  const [filter, setFilter] = useState([]);
+  return (
+    <div className="container">
+      {filter.length > 0 && (
+        <div className="filter-box">
+          {[...new Set(filter)].map((l, index) => {
+            return (
+              <button
+                className="filter-btn"
+                key={index}
+                onClick={() => {
+                  setFilter(filter.filter((e) => l !== e));
+                }}
+              >
+                {l}{" "}
+                <div className="img-close">
+                  <img src={close} alt="close" />
+                </div>
+              </button>
+            );
+          })}
+          <button
+            className="clear-btn"
+            onClick={() => {
+              setData(array);
+              setFilter([]);
+            }}
+          >
+            Clear
+          </button>
+        </div>
+      )}
+      <div className="lists-container">
+        {data.map((item, index) => {
+          return (
+            <div className={`list ${item.featured && "line"}`} key={index}>
+              <div className="img">
+                <img src={item.logo} alt="logo" />
+              </div>
+              <div className="other">
+                <div className="a">
+                  <h5>{item.company}</h5>
+                  {item.new && <NewBtn />}
+                  {item.featured && <FeaturedBtn />}
+                </div>
+                <h3>{item.position}</h3>
+                <p>
+                  <span>{item.postedAt}</span>
+                  <Stop />
+                  <span>{item.contract}</span>
+                  <Stop />
+                  <span>{item.location}</span>
+                </p>
+              </div>
+              <div className="filter">
+                <button
+                  className="filter-btn"
+                  onClick={() => {
+                    setData(data.filter((a) => a.role === item.role));
+                    setFilter([...filter, item.role]);
+                  }}
+                >
+                  {item.role}
+                </button>
+                <button
+                  className="filter-btn"
+                  onClick={() => {
+                    setData(data.filter((a) => a.level === item.level));
+                    setFilter([...filter, item.level]);
+                  }}
+                >
+                  {item.level}
+                </button>
+                {item.languages.map((e, index) => {
+                  return (
+                    <button
+                      className="filter-btn"
+                      key={index}
+                      onClick={() => {
+                        setData(data.filter((a) => a.languages.includes(e)));
+                        setFilter([...filter, e]);
+                      }}
+                    >
+                      {e}
+                    </button>
+                  );
+                })}
+                {item.tools &&
+                  item.tools.map((e, index) => {
+                    return (
+                      <button
+                        className="filter-btn"
+                        key={index}
+                        onClick={() => {
+                          setData(data.filter((a) => a.tools.includes(e)));
+                          setFilter([...filter, e]);
+                        }}
+                      >
+                        {e}
+                      </button>
+                    );
+                  })}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+const NewBtn = () => {
+  return <button className="btn new-btn">new!</button>;
+};
+const FeaturedBtn = () => {
+  return <button className="btn f-btn">featured</button>;
+};
+const Stop = () => {
+  return <span className="bold">.</span>;
+};
